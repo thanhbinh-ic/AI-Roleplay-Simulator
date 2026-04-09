c18e5d699a9ad4ee0451315ca3be4bc6ce0de4c6
Thanh Bình <82952557+thanhbinh-ic@users.noreply.github.com>
2026-04-09 13:20:25 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -448,12 +448,12 @@ const firebaseConfig =
   typeof __firebase_config !== "undefined"
     ? JSON.parse(__firebase_config)
     : {
-        apiKey: "YOUR_FALLBACK_API_KEY",
-        authDomain: "YOUR_FALLBACK_AUTH_DOMAIN",
-        projectId: "YOUR_FALLBACK_PROJECT_ID",
-        storageBucket: "YOUR_FALLBACK_STORAGE_BUCKET",
-        messagingSenderId: "YOUR_FALLBACK_MESSAGING_SENDER_ID",
-        appId: "YOUR_FALLBACK_APP_ID",
+        apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+        authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+        storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.REACT_APP_FIREBASE_APP_ID
       };
 
 const app = initializeApp(firebaseConfig);
