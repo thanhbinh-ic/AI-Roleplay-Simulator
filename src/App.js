355af133e46d4736a70b42c3d25fc0002ed4a69d
Thanh Bình <82952557+thanhbinh-ic@users.noreply.github.com>
2026-04-10 14:37:02 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useCallback, useRef } from "react";
 import { auth, db, googleProvider } from "./firebase";
-import { signInWithRedirect, getRedirectResult, onAuthStateChanged, signOut } from "firebase/auth";
+import { signInWithRedirect, onAuthStateChanged, signOut } from "firebase/auth";
 import './index.css';
 import {
   doc,
@@ -2856,16 +2856,6 @@ const App = () => {
       console.error("Lỗi đăng nhập:", error.message);
     }
   };
-
-  useEffect(() => {
-    getRedirectResult(auth)
-      .then((result) => {
-        if (result?.user) {
-          console.log("Đã đăng nhập sau khi redirect:", result.user.displayName);
-        }
-      })
-      .catch((error) => console.error("Lỗi Redirect:", error));
-  }, []);
 
   // 3. Hàm Đăng xuất
   const handleLogout = () => {
